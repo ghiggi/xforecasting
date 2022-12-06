e184c65ce43483047a9a18a5d3f1507c3548207a
ghiggi <gionata.ghiggi@gmail.com>
2022-12-06 18:02:47 +0100

    Run black

--- xforecasting/__init__.py
+++ xforecasting/__init__.py
@@ -5,17 +5,20 @@ Created on Thu Mar  3 17:06:20 2022
 
 @author: ghiggi
 """
-# TODO: use AR acronym for all 
-# file naming: dataloader_autoregressive --> dataloader 
-# file naming: training_autoregressive --> training 
-# file naming: predictions_autoregressive --> predictions 
+# TODO: use AR acronym for all
+# file naming: dataloader_autoregressive --> dataloader
+# file naming: training_autoregressive --> training
+# file naming: predictions_autoregressive --> predictions
 
 from .scheduler import AR_Scheduler
 from .dataloader_autoregressive import AutoregressiveDataset, AutoregressiveDataLoader
 from .training_autoregressive import AutoregressiveTraining
-from .predictions_autoregressive import AutoregressivePredictions, rechunk_forecasts_for_verification
+from .predictions_autoregressive import (
+    AutoregressivePredictions,
+    rechunk_forecasts_for_verification,
+)
 from .training_info import AR_TrainingInfo
-from .early_stopping import EarlyStopping 
+from .early_stopping import EarlyStopping
 
 from xforecasting import (
     AR_Scheduler,
@@ -37,5 +40,4 @@ __all__ = [
     "AutoregressivePredictions",
     "rechunk_forecasts_for_verification",
     "EarlyStopping",
-
 ]
